998b13667e62db33ec0dd4908076c2c7716a659b
Ramon B. Vollet <149214305+RamonVollet@users.noreply.github.com>
2026-05-21 16:54:35 -0300

    Add files via upload

---
+++ BibliotecaApp/repositories/EmprestimoRepository.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using BibliotecaApp.Models;
+using Dapper;
+using MySqlConnector;
+
+namespace BibliotecaApp.Repositories
+{
+    public class EmprestimoRepository : IRepository<Emprestimo>
+    {
+        public void Inserir(Emprestimo entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = @"INSERT INTO Emprestimo (LivroId, UsuarioId, DataEmprestimo, DataDevolucao) 
+                               VALUES (@LivroId, @UsuarioId, @DataEmprestimo, @DataDevolucao)";
+                conexao.Execute(sql, entidade);
+            }
+        }
+
+        public IEnumerable<Emprestimo> BuscarTodos()
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                return conexao.Query<Emprestimo>("SELECT * FROM Emprestimo");
+            }
+        }
+        public void Editar(Emprestimo entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = @"UPDATE Emprestimo 
+                               SET LivroId = @LivroId, UsuarioId = @UsuarioId, 
+                                   DataEmprestimo = @DataEmprestimo, DataDevolucao = @DataDevolucao 
+                               WHERE Id = @Id";
+                conexao.Execute(sql, entidade);
+            }
+        }
+        public void Excluir(int id)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "DELETE FROM Emprestimo WHERE Id = @Id";
+                conexao.Execute(sql, new { Id = id });
+            }
+        }
+    }
+}
