2684ece74c966108c91719cf3d035ef6f02934fc
Ian Souza <isouzad8@gmail.com>
2026-05-21 12:49:00 -0300

    Add files via upload

---
+++ BibliotecaApp/Views/FormUsuario.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace BibliotecaApp.Views
+{
+    partial class FormUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_nome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.lbl_cpf = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lbl_matricula = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.lbl_telefone = new System.Windows.Forms.Label();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_nome
+            // 
+            this.lbl_nome.AutoSize = true;
+            this.lbl_nome.Location = new System.Drawing.Point(13, 13);
+            this.lbl_nome.Name = "lbl_nome";
+            this.lbl_nome.Size = new System.Drawing.Size(82, 13);
+            this.lbl_nome.TabIndex = 0;
+            this.lbl_nome.Text = "Nome Completo";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 30);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // lbl_cpf
+            // 
+            this.lbl_cpf.AutoSize = true;
+            this.lbl_cpf.Location = new System.Drawing.Point(16, 57);
+            this.lbl_cpf.Name = "lbl_cpf";
+            this.lbl_cpf.Size = new System.Drawing.Size(27, 13);
+            this.lbl_cpf.TabIndex = 2;
+            this.lbl_cpf.Text = "CPF";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(16, 74);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(100, 20);
+            this.txtCpf.TabIndex = 3;
+            // 
+            // lbl_matricula
+            // 
+            this.lbl_matricula.AutoSize = true;
+            this.lbl_matricula.Location = new System.Drawing.Point(16, 101);
+            this.lbl_matricula.Name = "lbl_matricula";
+            this.lbl_matricula.Size = new System.Drawing.Size(108, 13);
+            this.lbl_matricula.TabIndex = 4;
+            this.lbl_matricula.Text = "Matrícula Acadêmica";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(16, 118);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtMatricula.TabIndex = 5;
+            // 
+            // lbl_telefone
+            // 
+            this.lbl_telefone.AutoSize = true;
+            this.lbl_telefone.Location = new System.Drawing.Point(16, 145);
+            this.lbl_telefone.Name = "lbl_telefone";
+            this.lbl_telefone.Size = new System.Drawing.Size(49, 13);
+            this.lbl_telefone.TabIndex = 6;
+            this.lbl_telefone.Text = "Telefone";
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(16, 162);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 7;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(16, 188);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(100, 23);
+            this.btnSalvar.TabIndex = 8;
+            this.btnSalvar.Text = "Salvar Usuário";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(130, 13);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(544, 227);
+            this.dgvUsuarios.TabIndex = 9;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(16, 217);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 23);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir Usuário";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // FormUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.lbl_telefone);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.lbl_matricula);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.lbl_cpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lbl_nome);
+            this.Name = "FormUsuario";
+            this.Text = "FormUsuario";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_nome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label lbl_cpf;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.Label lbl_matricula;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.Label lbl_telefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
---
+++ BibliotecaApp/Views/FormUsuario.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using BibliotecaApp.Models;
+using BibliotecaApp.Repositories;
+
+namespace BibliotecaApp.Views
+{
+    public partial class FormUsuario : Form
+    {
+        private UsuarioRepository _usuarioRepository = new UsuarioRepository();
+        private int _idSelecionado = 0;
+
+        public FormUsuario()
+        {
+            InitializeComponent();
+            this.Load += FormUsuario_Load;
+            btnExcluir.Click += btnExcluir_Click;
+            dgvUsuarios.CellClick += dgvUsuarios_CellClick;
+        }
+
+        private void FormUsuario_Load(object sender, EventArgs e)
+        {
+            AtualizarGrid();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtNome.Text) || string.IsNullOrWhiteSpace(txtCpf.Text))
+            {
+                MessageBox.Show("Nome e CPF são obrigatórios!", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!ValidarCpf(txtCpf.Text))
+            {
+                MessageBox.Show("O CPF digitado é inválido!", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!string.IsNullOrWhiteSpace(txtTelefone.Text) && !ValidarTelefone(txtTelefone.Text))
+            {
+                MessageBox.Show("O número de telefone digitado é inválido! Insira um formato válido com DDD.", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var usuario = new Usuario
+                {
+                    Id = _idSelecionado,
+                    Nome = txtNome.Text,
+                    Cpf = txtCpf.Text,
+                    Matricula = txtMatricula.Text,
+                    Telefone = txtTelefone.Text
+                };
+
+                if (_idSelecionado == 0)
+                {
+                    _usuarioRepository.Inserir(usuario);
+                    MessageBox.Show("Usuário/Leitor registrado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    _usuarioRepository.Editar(usuario);
+                    MessageBox.Show("Dados do usuário atualizados com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                LimparCampos();
+                AtualizarGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao salvar no banco: " + ex.Message, "Erro Crítico", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvUsuarios_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dgvUsuarios.CurrentRow != null)
+            {
+                var usuarioSelecionado = dgvUsuarios.CurrentRow.DataBoundItem as Usuario;
+
+                if (usuarioSelecionado != null)
+                {
+                    _idSelecionado = usuarioSelecionado.Id;
+                    txtNome.Text = usuarioSelecionado.Nome;
+                    txtCpf.Text = usuarioSelecionado.Cpf;
+                    txtMatricula.Text = usuarioSelecionado.Matricula;
+                    txtTelefone.Text = usuarioSelecionado.Telefone;
+
+                    btnSalvar.Text = "Atualizar Usuário";
+                }
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (_idSelecionado == 0)
+            {
+                MessageBox.Show("Selecione um usuário na tabela antes de tentar excluir!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var resultado = MessageBox.Show("Tem certeza que deseja excluir este usuário/leitor definitivamente?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                try
+                {
+                    _usuarioRepository.Excluir(_idSelecionado);
+                    MessageBox.Show("Usuário removido com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    LimparCampos();
+                    AtualizarGrid();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao excluir: {ex.Message}. Verifique se este usuário não possui empréstimos pendentes vinculados a ele.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LimparCampos()
+        {
+            _idSelecionado = 0;
+            txtNome.Clear();
+            txtCpf.Clear();
+            txtMatricula.Clear();
+            txtTelefone.Clear();
+            btnSalvar.Text = "Registrar Leitor";
+        }
+
+        private void RazaoGrid()
+        {
+            try
+            {
+                dgvUsuarios.DataSource = null;
+                dgvUsuarios.Columns.Clear();
+                dgvUsuarios.AutoGenerateColumns = false;
+
+                dgvUsuarios.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Id", HeaderText = "ID", Width = 50 });
+                dgvUsuarios.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Nome", HeaderText = "Nome", Width = 150 });
+                dgvUsuarios.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Cpf", HeaderText = "CPF", Width = 100 });
+                dgvUsuarios.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Matricula", HeaderText = "Matrícula", Width = 90 });
+                dgvUsuarios.Columns.Add(new DataGridViewTextBoxColumn { DataPropertyName = "Telefone", HeaderText = "Telefone", Width = 110 });
+
+                dgvUsuarios.DataSource = _usuarioRepository.BuscarTodos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao atualizar tabela: " + ex.Message);
+            }
+        }
+
+        private void AtualizarGrid()
+        {
+            RazaoGrid();
+        }
+
+        private bool ValidarCpf(string cpf)
+        {
+            cpf = Regex.Replace(cpf, @"[^\d]", "");
+
+            if (cpf.Length != 11) return false;
+
+            bool todosIguais = true;
+            for (int i = 1; i < 11; i++)
+            {
+                if (cpf[i] != cpf[0]) todosIguais = false;
+            }
+            if (todosIguais) return false;
+
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            string tempCpf = cpf.Substring(0, 9);
+            int soma = 0;
+
+            for (int i = 0; i < 9; i++) soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+            int resto = soma % 11;
+            int digito1 = resto < 2 ? 0 : 11 - resto;
+
+            tempCpf = tempCpf + digito1;
+            soma = 0;
+
+            for (int i = 0; i < 10; i++) soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+            resto = soma % 11;
+            int digito2 = resto < 2 ? 0 : 11 - resto;
+
+            return cpf.EndsWith(digito1.ToString() + digito2.ToString());
+        }
+
+        private bool ValidarTelefone(string telefone)
+        {
+            string numeroLimpo = Regex.Replace(telefone, @"[^\d]", "");
+            return numeroLimpo.Length == 10 || numeroLimpo.Length == 11;
+        }
+    }
+}
