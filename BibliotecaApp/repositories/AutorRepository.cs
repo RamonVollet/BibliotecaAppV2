a82b01b9ddc6249b44728d56c391c1d42db8fc50
Marco-Aurelio-Silva-Lima <maslima@uniara.edu.br>
2026-05-21 13:36:10 -0300

    Add files via upload

---
+++ BibliotecaApp/repositories/AutorRepository.cs
@@ -0,0 +1,45 @@
+﻿using BibliotecaApp.Models;
+using Dapper;
+using MySqlConnector;
+using System.Collections.Generic;
+
+namespace BibliotecaApp.Repositories
+{
+    public class AutorRepository : IRepository<Autor>
+    {
+        public void Inserir(Autor entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "INSERT INTO Autor (Nome, Nacionalidade) VALUES (@Nome, @Nacionalidade)";
+                conexao.Execute(sql, entidade);
+            }
+        }
+
+        public IEnumerable<Autor> BuscarTodos()
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                return conexao.Query<Autor>("SELECT * FROM Autor");
+            }
+        }
+
+        public void Editar(Autor entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "UPDATE Autor SET Nome = @Nome, Nacionalidade = @Nacionalidade WHERE Id = @Id";
+                conexao.Execute(sql, entidade);
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "DELETE FROM Autor WHERE Id = @Id";
+                conexao.Execute(sql, new { Id = id });
+            }
+        }
+    }
+}
