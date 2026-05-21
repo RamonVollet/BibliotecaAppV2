11667e37fe45d44fe7ae9c4356be3fc158c35d5f
Ramon B. Vollet <149214305+RamonVollet@users.noreply.github.com>
2026-05-21 16:54:05 -0300

    Add files via upload

---
+++ BibliotecaApp/models/Emprestimo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaApp.Models
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+        public int LivroId { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime DataEmprestimo { get; set; }
+        public DateTime? DataDevolucao { get; set; }
+    }
+}
