a0f6836d5ebd8cbc8fd7f6c3306bef024e5005ff
Marco-Aurelio-Silva-Lima <maslima@uniara.edu.br>
2026-05-21 13:35:32 -0300

    Add files via upload

---
+++ BibliotecaApp/models/Autor.cs
@@ -0,0 +1,9 @@
+namespace BibliotecaApp.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Nacionalidade { get; set; }
+    }
+}
