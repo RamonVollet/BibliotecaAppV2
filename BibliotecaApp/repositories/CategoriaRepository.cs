85be268fcd21d147a1c54e9f56c49988b864d08c
Isaac Da Costa Melo <isaaccostaa344@gmail.com>
2026-05-21 15:56:45 -0300

    Add files via upload

---
+++ BibliotecaApp/repositories/CategoriaRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using BibliotecaApp.Models;
+using Dapper;
+using MySqlConnector;
+
+namespace BibliotecaApp.Repositories
+{
+    public class CategoriaRepository : IRepository<Categoria>
+    {
+        public void Inserir(Categoria entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "INSERT INTO Categoria (Descricao) VALUES (@Descricao)";
+                conexao.Execute(sql, entidade);
+            }
+        }
+
+        public IEnumerable<Categoria> BuscarTodos()
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                return conexao.Query<Categoria>("SELECT * FROM Categoria");
+            }
+        }
+
+        public void Editar(Categoria entidade)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "UPDATE Categoria SET Descricao = @Descricao WHERE Id = @Id";
+                conexao.Execute(sql, entidade);
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            using (var conexao = new MySqlConnection(Config.StringConexao))
+            {
+                string sql = "DELETE FROM Categoria WHERE Id = @Id";
+                conexao.Execute(sql, new { Id = id });
+            }
+        }
+    }
+}
