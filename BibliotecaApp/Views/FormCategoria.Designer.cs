b18c87559e5208da51ad405268d91367ef328fd0
Isaac Da Costa Melo <isaaccostaa344@gmail.com>
2026-05-21 15:58:15 -0300

    Add files via upload

---
+++ BibliotecaApp/Views/FormCategoria.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace BibliotecaApp.Views
+{
+    partial class FormCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_categoria = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.dgvCategorias = new System.Windows.Forms.DataGridView();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_categoria
+            // 
+            this.lbl_categoria.AutoSize = true;
+            this.lbl_categoria.Location = new System.Drawing.Point(13, 13);
+            this.lbl_categoria.Name = "lbl_categoria";
+            this.lbl_categoria.Size = new System.Drawing.Size(118, 13);
+            this.lbl_categoria.TabIndex = 0;
+            this.lbl_categoria.Text = "Descrição da Categoria";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(16, 30);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(100, 20);
+            this.txtDescricao.TabIndex = 1;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(16, 57);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(100, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar Categoria";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // dgvCategorias
+            // 
+            this.dgvCategorias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategorias.Location = new System.Drawing.Point(150, 12);
+            this.dgvCategorias.Name = "dgvCategorias";
+            this.dgvCategorias.Size = new System.Drawing.Size(245, 231);
+            this.dgvCategorias.TabIndex = 3;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(16, 87);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(100, 23);
+            this.btnExcluir.TabIndex = 4;
+            this.btnExcluir.Text = "Excluir Categoria";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // FormCategoria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.dgvCategorias);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.lbl_categoria);
+            this.Name = "FormCategoria";
+            this.Text = "FormCategoria";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategorias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_categoria;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.DataGridView dgvCategorias;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
---
+++ BibliotecaApp/Views/FormCategoria.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Windows.Forms;
+using BibliotecaApp.Models;
+using BibliotecaApp.Repositories;
+
+namespace BibliotecaApp.Views
+{
+    public partial class FormCategoria : Form
+    {
+        private CategoriaRepository _categoriaRepository = new CategoriaRepository();
+        private int _idSelecionado = 0;
+
+        public FormCategoria()
+        {
+            InitializeComponent();
+            this.Load += FormCategoria_Load;
+            btnExcluir.Click += btnExcluir_Click;
+            dgvCategorias.CellClick += dgvCategorias_CellClick;
+        }
+
+        private void FormCategoria_Load(object sender, EventArgs e)
+        {
+            AtualizarGrid();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDescricao.Text))
+            {
+                MessageBox.Show("A descrição é obrigatória!", "Validação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                var categoria = new Categoria
+                {
+                    Id = _idSelecionado,
+                    Descricao = txtDescricao.Text
+                };
+
+                if (_idSelecionado == 0)
+                {
+                    _categoriaRepository.Inserir(categoria);
+                    MessageBox.Show("Categoria salva com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    _categoriaRepository.Editar(categoria);
+                    MessageBox.Show("Categoria atualizada com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                LimparCampos();
+                AtualizarGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao salvar no banco: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvCategorias_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && dgvCategorias.CurrentRow != null)
+            {
+                var categoriaSelecionada = dgvCategorias.CurrentRow.DataBoundItem as Categoria;
+
+                if (categoriaSelecionada != null)
+                {
+                    _idSelecionado = categoriaSelecionada.Id;
+                    txtDescricao.Text = categoriaSelecionada.Descricao;
+                    btnSalvar.Text = "Atualizar Categoria";
+                }
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (_idSelecionado == 0)
+            {
+                MessageBox.Show("Selecione uma categoria na tabela antes de tentar excluir!", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var resultado = MessageBox.Show("Tem certeza que deseja excluir esta categoria?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (resultado == DialogResult.Yes)
+            {
+                try
+                {
+                    _categoriaRepository.Excluir(_idSelecionado);
+                    MessageBox.Show("Categoria removida com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    LimparCampos();
+                    AtualizarGrid();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao excluir: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void LimparCampos()
+        {
+            _idSelecionado = 0;
+            txtDescricao.Clear();
+            btnSalvar.Text = "Salvar Categoria";
+        }
+
+        private void AtualizarGrid()
+        {
+            try
+            {
+                dgvCategorias.DataSource = _categoriaRepository.BuscarTodos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
